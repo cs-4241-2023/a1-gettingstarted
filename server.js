c7f17838f5b85abecd472b85c20f65a2f8fa294e
Andrew Hariyanto <51850304+andrewhariyanto@users.noreply.github.com>
2023-08-28 13:42:35 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
+//loading and importing in http library
 const http = require('http'),
+      //need fs module for sending and reading files
       fs   = require('fs'),
       port = 3000
 
+//create server. Server needs a request and response for a client. Whenever a server is created, it needs a request and will output a response
 const server = http.createServer( function( request,response ) {
-  switch( request.url ) {
-    case '/':
-      sendFile( response, 'index.html' )
-      break
-    case '/index.html':
+      //request object contains info like url, etc.
+      //response object contains info like error messsages
+      const url = request.url.slice(1)
+  switch( url ) {
+    case '':
       sendFile( response, 'index.html' )
       break
     default:
-      response.end( '404 Error: File Not Found' )
+      sendFile(response, url); break;
   }
 })
 
 server.listen( process.env.PORT || port )
 
 const sendFile = function( response, filename ) {
-   fs.readFile( filename, function( err, content ) {
-     response.end( content, 'utf-8' )
-   })
+      fs.readFile( filename, function( err, content ) {
+            if(error ===null){
+                  response.end('file not found')
+      }else{
+            //send content and type of content (utf-8) to response
+            response.end( content, 'utf-8' )
+      }
+})
 }
