a6a8cf9fcd8cca4219e5e926030baef199e1ab61
SmeeBoi <sameer.desai.boston@gmail.com>
2023-08-30 13:42:59 -0400

    added cool cursor. hover over text still broken

--- README.md
+++ README.md
@@ -3,6 +3,32 @@ Sameer Desai https://sameer-test-glitch.glitch.me/
 This project shows ...
 
 
+Technical Achievements:
 
-Added Josefin Sans from Google Fonts
+CSS:
+    1)
+    2)
+    3)
+    4)
+    5)
+
+JavaScript Animation:
+1) Created circle cursor with slight delay animation
+
+HTML tags
+1)
+2)
+3)
+4)
+5)
+
+
+
+Design Achievements:
+1) Used color palette created with Adobe:
+
+
+2) Added Josefin Sans from Google Fonts: https://fonts.google.com/specimen/Josefin+Sans?query=josefin+sans
+
+
 
---
+++ script.js
@@ -0,0 +1,19 @@
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", (e) => {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursorOutline.animate({
+            left: `${posX}px`,
+            top: `${posY}px`,
+        }, { duration: 400, fill: "forwards" })
+        // https: //www.youtube.com/watch?v=UMdvufdewD8
+
+
+
+});
