d6fc7b7749d7b74e8e20e9564bf1abfa9efeb77e
kristine-guan <kristineguan24@gmail.com>
2023-08-31 01:45:44 -0400

    refactor again
    rename server.js

--- server.js
+++ server.js
@@ -1,29 +1,4 @@
-// const http = require('http'),
-//       fs   = require('fs'),
-//       port = 3000
 
-// const server = http.createServer( function( request,response ) {
-//   switch( request.url ) {
-//     case '/':
-//       sendFile( response, 'index.html' )
-//       break
-//     case '/index.html':
-//       sendFile( response, 'index.html' )
-//       break
-//     default:
-//       response.end( '404 Error: File Not Found!' )
-//   }
-// })
-
-// server.listen( process.env.PORT || port )
-
-// const sendFile = function( response, filename ) {
-//    fs.readFile( filename, function( err, content ) {
-//      response.end( content, 'utf-8' )
-//    })
-// }
-
-// in class 8/28 -----------------------------------------------------------------------
 
 const http = require("http"),
         fs = require("fs")
@@ -55,39 +30,3 @@ const sendFile = function (filename, response) {
     response.end( content, 'utf-8')
   }})
 };
-
-// this one better than one in porject? can pick which one is better i guess
-// dont use == in JS 
-
-
-// from office hours 
-
-// const http = require("http"),
-//   fs = require("fs");
-
-// const sendFile = function (filename, response) {
-//   fs.readFile(filename, (err, content) => {
-//     if (err !== null) {
-//       console.log(err);
-//       response.end("file not founddd");
-//     } else {
-//       response.end(content, "utf-8");
-//     }
-//   });
-// };
-
-// const server = http.createServer(function (request, response) {
-//   switch (request.url) {
-//     case "/":
-//       sendFile(response, "index.html");
-//       break;
-//     case "/index.html":
-//       sendFile(response, "index.html");
-//       break;
-//     case "/styles.css":
-//       sendFile(response, "style.css");
-//       break;
-//     default:
-//       response.end("404 Error: File Not Found");
-//   }
-// });
